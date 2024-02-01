d55bb192feb55f7828ac5a92fc595c5771462dc7
shubhmalhotra001 <42601812+shubhmalhotra001@users.noreply.github.com>
2024-02-01 18:36:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+
+import argparse
+from network import Network
+
+def main(n, z0, z1, txn_time, mining_time, simulation_until):
+    network = Network(n, z0, z1)
+    network.display_network()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', type=int, help='No. of nodes')
+    parser.add_argument('z1', type=float, help='Fraction of low cpu peers')
+    parser.add_argument('txn_time', type=int, help='Transaction time in ms')
+    parser.add_argument('mining_time', type=int, help='Mining time in ms')
+    parser.add_argument('simulation_until', type=int, help='Simulation time units')
+
+    args = parser.parse_args()
+   
+    if (5 <= len(vars(args)) < 5):
+        parser.error("Provide 5 arguments:\n"
+                     "No. of nodes\n"
+                     "Fraction of low cpu peers\n"
+                     "Transaction time in ms\n"
+                     "Mining time in ms\n"
+                     "Simulation time units\n"
+                    )
+
+    n = args.n
+    z0 = 0.5 
+    z1 = args.z1
+    txn_time = args.txn_time
+    mining_time = args.mining_time
+    simulation_until = args.simulation_until
+
+    print("n:", n)
+    print("z1:", z1)
+    print("txn_time:", txn_time)
+    print("mining_time:", mining_time)
+    print("simulation_until:", simulation_until)
+
+    main(n, z0, z1, txn_time, mining_time, simulation_until)
+
+
---
+++ network.py
@@ -0,0 +1,125 @@
+import random
+import string
+import numpy as np
+from collections import deque
+
+
+class Peer:
+    def __init__(self, peer_id, is_slow, is_low_cpu):
+        self.peer_id = peer_id
+        self.is_slow = is_slow
+        self.is_low_cpu = is_low_cpu
+        self.coins = 1000  # Initial coins
+        self.neighbors = set()
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.add(neighbor)
+
+    def remove_neighbor(self, neighbor):
+        if neighbor in self.neighbors:
+            self.neighbors.remove(neighbor)
+
+    def get_neighbors(self):
+        return list(self.neighbors)
+
+    def generate_transaction(self, peers):
+        txn_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        receiver = random.choice(peers)
+        amount = random.randint(1, self.coins)
+        if amount <= self.coins:
+            self.coins -= amount
+            return f"TxnID: {txn_id}, {self.peer_id} pays {receiver.peer_id} {amount} coins"
+        else:
+            return None
+        
+
+class Network:
+    def __init__(self, n, z0, z1):
+        self.n = n
+        self.z0 = z0
+        self.z1 = z1
+        self.peers = []
+        self.neighbor_constraint = False
+        self.connected_graph = False
+
+        self.create_peers()
+        self.create_network_topology()
+
+    def create_peers(self):
+        slow_peers, low_cpu_peers = 0, 0
+        for i in range(self.n):
+            speed_threshold = np.random.uniform(0, 1)
+            cpu_threshold = np.random.uniform(0, 1)
+            is_slow = speed_threshold <= self.z0
+            is_low_cpu = cpu_threshold <= self.z1
+            if is_slow:
+                slow_peers += 1
+            if is_low_cpu:
+                low_cpu_peers += 1
+            peer = Peer(i, is_slow, is_low_cpu)
+            self.peers.append(peer)
+
+        fast_peers = self.n - slow_peers
+        high_cpu_peers = self.n - low_cpu_peers
+
+
+    def build_neighbors(self, peer):
+        num_neighbors = random.randint(3, 6)
+        # print(f"{peer.peer_id} NEIGHBORS: {num_neighbors}")
+        available_peers = [p for p in self.peers if p != peer and len(p.neighbors) < 6 and p not in peer.neighbors]
+        random.shuffle(available_peers)
+        
+        for _ in range(num_neighbors - len(peer.neighbors)):
+            if available_peers:
+                neighbor = available_peers.pop()
+                # print(f"{neighbor.peer_id} added as neighbor of {peer.peer_id}")
+                peer.add_neighbor(neighbor)
+                neighbor.add_neighbor(peer)
+            else:
+                return
+            
+        self.neighbor_constraint = True
+    
+    def reset_network(self):
+        for peer in self.peers:
+            peer.neighbors.clear()
+        self.neighbor_constraint = False
+        self.connected_graph = False
+
+    def create_network_topology(self):
+        print("Building network... \n")
+        while not (self.connected_graph and self.neighbor_constraint):
+            self.reset_network()
+
+            for peer in self.peers:
+                self.build_neighbors(peer)
+
+            if not (self.neighbor_constraint):
+                print("Not every node has 3-6 neighbors, rebuilding... \n")
+                continue
+
+            self.connected_graph = self.is_connected_graph()
+            if not (self.connected_graph):
+                print("Network topology not connected, rebuilding... \n")
+
+        print("Network created \n")
+
+    def is_connected_graph(self):
+        visited = set()
+        queue = deque([self.peers[0]]) 
+        while queue:
+            current_peer = queue.popleft()
+            visited.add(current_peer)
+            for neighbor in current_peer.get_neighbors():
+                if neighbor not in visited:
+                    queue.append(neighbor)
+        return len(visited) == len(self.peers)
+    
+    def display_network(self):
+        for peer in self.peers:
+            neighbors = peer.get_neighbors()
+            neighbor_ids = [neighbor.peer_id for neighbor in neighbors]
+            print(f"Peer {peer.peer_id} is connected to: {neighbor_ids}")
+
+
+
