3408bd668b1adea2cb78751b230613d2dd5bd274
Shubh Malhotra <shubhmalhotra001@gmail.com>
2024-02-17 20:03:47 +0530

    Added comments for transaction

--- transaction.py
+++ transaction.py
@@ -3,14 +3,15 @@ from uuid import uuid4
 
 class Transaction:
     def __init__(self, ts, amount, sender_id, receiver_id):
+        """"method to initialize attributes of transaction"""
         self.id = uuid4()
         self.timestamp = ts
         self.sender_id = sender_id
         self.receiver_id = receiver_id
         self.amount = float(amount)
 
-    def __str__(self): 
-        if self.sender_id == None:
+    def __str__(self):
+        if self.sender_id is None:
             return (f"{self.id}: {self.receiver_id} mines {self.amount} coins")
         return (f"{self.id}: {self.sender_id} pays {self.receiver_id} {self.amount} coins")                 
            
